fe16d381a730561d86015b499b93c64b72b6e82c
yangfeng <1041114134@qq.com>
2019-01-02 12:30:23 +0800

    扫描service

--- redis_provider/src/main/java/com/jp/common/redis/provider/RedisProviderApplication.java
+++ redis_provider/src/main/java/com/jp/common/redis/provider/RedisProviderApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource({"classpath:conf/*.xml" })
+@ComponentScan({"com.jp.common.redis.provider"})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RedisProviderApplication {
 
